386474c23a1f204cfd0d8e448c604bcd96d8140a
pablop76 <pawelpoltoraczyk@gmail.com>
2020-12-05 22:35:19 +0100

    podlinkowanie flag

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-import UIkit from "uikit/dist/js/uikit.min";
-import Icons from "uikit/dist/js/uikit-icons.min";
-// loads the Icon plugin
-UIkit.use(Icons);
+import "./spine";
---
+++ spine.js
@@ -0,0 +1,4 @@
+import UIkit from "uikit/dist/js/uikit.min";
+import Icons from "uikit/dist/js/uikit-icons.min";
+// loads the Icon plugin
+UIkit.use(Icons);
